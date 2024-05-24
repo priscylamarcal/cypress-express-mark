09cca52758dfd067a74f16bf35ae6dfe89422a97
priscylamarcal <primarcal_@hotmail.com>
2024-05-23 22:15:09 -0300

    alterações

--- cypress/e2e/tasks.cy.js
+++ cypress/e2e/tasks.cy.js
@@ -3,7 +3,7 @@ describe('tarefas', () => {
         cy.visit('http://localhost:3000');
 
         cy.get('input[placeholder="Add a new Task"]').type(
-            'Ler um livro de Node.js'
+            'Ler um livro de Node.jss'
         );
 
         cy.contains('button', 'Create').click();
